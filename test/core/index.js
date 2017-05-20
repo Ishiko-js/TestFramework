dbe391a912380f257cb8fcdd2f99d8cdb9c2efe8
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-05-20 11:54:13 +0100

    Updates

--- test/core/index.js
+++ test/core/index.js
@@ -3,6 +3,6 @@
 var tf = require("../../lib/index.js")
 
 let tests = new tf.TestHarness()
-let testSequence = tests.appendTestSequence()
+let testSequence = tests.appendTestSequence("ishiko-js/test-framework/core tests")
 testSequence.append(new tf.Test("Test1"))
 tests.run()
