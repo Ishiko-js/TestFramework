750c062cb4da759956516f7774ee0f915b1c049d
Xavier Leclercq <xavier.leclercq@needfulsoftware.com>
2017-05-20 16:58:36 +0100

    Updates

---
+++ test/core/FunctionBasedTestTests.js
@@ -0,0 +1,55 @@
+'use strict'
+
+var tf = require("../../lib/index.js")
+
+module.exports = function(theTestHarness) {
+    let testSequence = theTestHarness.appendTestSequence("FunctionBasedTest tests")
+
+    new tf.FunctionBasedTest("Creation test 1", FunctionBasedTestCreationTest1, testSequence)
+    new tf.FunctionBasedTest("run success test 1", FunctionBasedTestRunSuccessTest1, testSequence)
+    new tf.FunctionBasedTest("run failure test 1", FunctionBasedTestRunFailureTest1, testSequence)
+}
+
+function FunctionBasedTestCreationTest1()
+{
+    let test = new tf.FunctionBasedTest(
+        "FunctionBasedTestCreationTest1", 
+        function() {
+            return tf.TestResultOutcome.ePassed
+        });
+    return tf.TestResultOutcome.ePassed;
+}
+
+function FunctionBasedTestRunSuccessTest1()
+{
+    let test = new tf.FunctionBasedTest(
+        "FunctionBasedTestRunSuccessTest1", 
+        function() {
+            return tf.TestResultOutcome.ePassed
+        });
+
+    test.run()
+
+    if (test.result.outcome == tf.TestResultOutcome.ePassed) {
+        return tf.TestResultOutcome.ePassed
+    } else {
+        return tf.TestResultOutcome.eFailed;
+    }
+}
+
+function FunctionBasedTestRunFailureTest1()
+{
+    let test = new tf.FunctionBasedTest(
+        "FunctionBasedTestRunSuccessTest1", 
+        function() {
+            return tf.TestResultOutcome.eFailed
+        });
+
+    test.run()
+
+    if (test.result.outcome == tf.TestResultOutcome.eFailed) {
+        return tf.TestResultOutcome.ePassed
+    } else {
+        return tf.TestResultOutcome.eFailed;
+    }
+}
--- test/core/index.js
+++ test/core/index.js
@@ -2,9 +2,11 @@
 
 var tf = require("../../lib/index.js")
 var AddTestTests = require("./TestTests.js")
+var AddFunctionBasedTestTests = require("./FunctionBasedTestTests.js")
 
 let theTestHarness = new tf.TestHarness("ishiko-js/test-framework/core")
 
 AddTestTests(theTestHarness);
+AddFunctionBasedTestTests(theTestHarness);
 
 theTestHarness.run()
